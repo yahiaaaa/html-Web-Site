7110667fb1dc8ee586a22e1b7cb31111589a0b3a
yahia0saleh <>
2021-06-30 15:12:06 +0000

    java script

---
+++ app.js
@@ -0,0 +1,11 @@
+confirm("wlecome to my website");
+
+ var name = prompt("what type of pets have ?")
+ console.log(name)
+
+
+ if (name == "cat")
+ {document.write("<h4>" + name + "</h4>")}
+ else if(name != "cat"){
+   alert("i cant help you ")
+ }
