503de75114584f3c4a676262922b0392f26fa277
yahia0saleh <>
2021-07-04 14:37:50 +0000

    function

---
+++ fun.js
@@ -0,0 +1,19 @@
+confirm("wlecome to my website");
+
+var num ;
+var cost = 10 ;
+var num = prompt("how many of pets have ?")
+//console.log(num * cost)
+
+function mul ( num , cost ){
+  var mul = (num*cost)
+
+//consol.log (mul)
+
+document.write("cost  =  " + mul ) ;
+return mul ;
+
+
+}
+mul(num , cost) ;
+
